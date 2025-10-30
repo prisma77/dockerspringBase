816eb38d349251b1f8e5a2c0b228c6aa6fae4165
Prisma <prisma@kakao.com>
2025-10-30 11:39:40 +0900

    Initial commit

---
+++ src/main/java/com/multi/dockerspringbase/DockerspringBaseApplication.java
@@ -0,0 +1,13 @@
+package com.multi.dockerspringbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerspringBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerspringBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multi/dockerspringbase/DockerspringBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.dockerspringbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerspringBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
